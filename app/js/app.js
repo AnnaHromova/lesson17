c808eba80aa1ffa55d833b0f63d474ce77de1f38
AnnaHromova <77169668+AnnaHromova@users.noreply.github.com>
2021-02-16 12:58:35 +0200

    1
    1

--- app/js/app.js
+++ app/js/app.js
@@ -12,6 +12,21 @@ $(document).ready(function () {
     },
   });
 
+  //Login modal
+  $('.js-login').on('click', function () {
+    $('.modal--login').fadeIn(400);
+  });
 
+  $('.modal__close').on('click', function () {
+    $('.modal--login').fadeOut(400);
+  });
+
+  $(document).on('mouseup', function (event) {
+    const LoginModal = $('.modal__window');
+    if (!LoginModal.is(event.target) && LoginModal.has(event.target).length === 0) {
+      $('.modal').fadeOut(400);
+    }
+
+
+  });
 })
-
